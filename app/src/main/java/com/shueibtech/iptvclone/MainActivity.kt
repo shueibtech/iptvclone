2ca30e35a2c09b837889f47bac0ee337c589dfda
tec7on <tec7on@proton.me>
2026-06-30 20:13:33 +0300

    Initial commit: Iptv Clone

---
+++ app/src/main/java/com/shueibtech/iptvclone/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.shueibtech.iptvclone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.shueibtech.iptvclone.ui.theme.IptvCloneTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            IptvCloneTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HelloWorld()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HelloWorld() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello World",
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HelloWorldPreview() {
+    IptvCloneTheme {
+        HelloWorld()
+    }
+}
---
+++ app/src/main/java/com/shueibtech/iptvclone/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.shueibtech.iptvclone.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Accent     = Color(0xFF19F793)
+val White      = Color(0xFFFFFFFF)
+val Black      = Color(0xFF000000)
